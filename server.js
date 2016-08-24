ee3df686951ea6febd4b451642b0cd6a60158f89
Rachel Adler <adler.rachel.a@gmail.com>
2016-08-23 21:42:05 -0400

    add index.js with route rules

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+var rules = require('./app');
+app.use(rules);
+
 var port = "8080";
 var host = "0.0.0.0";
 
 app.listen(port, host);
+
 console.log("Listening on 0.0.0.0:8080... ");
