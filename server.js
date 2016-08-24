819c091038409236baaed0f154961ad44f351c8d
Rachel Adler <adler.rachel.a@gmail.com>
2016-08-23 21:06:55 -0400

    add server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-require('express');
+var express = require('express');
+var app = express();
+
+var port = "8080";
+var host = "0.0.0.0";
+
+app.listen(port, host);
+console.log("Listening on 0.0.0.0:8080... ");
